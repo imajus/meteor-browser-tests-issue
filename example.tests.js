344dbbcba676dc5cbf7a7798569701cfff170ecd
Denis Perov <denis.perov@gmail.com>
2024-09-20 08:34:53 +0300

    Add console logging

--- example.tests.js
+++ example.tests.js
@@ -1,6 +1,8 @@
 import assert from 'assert';
 describe('Example', () => {
+  console.log('In the test group');
   it('test', () => {
+    console.log('In the test case');
     assert.fail();
   });
 });
